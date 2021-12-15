5ec81a64769c211878c8b2ed7fe7e70ea403e0c4
RobertaMatera <materaroberta11@gmail.com>
2021-12-15 20:42:50 +0100

    Warning Danger Alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNav from './Components/MyNav';
+import WarningSign from './Components/WarningSign';
 
 function App() {
   return (
     <div className="App">
       <MyNav></MyNav>
+      <WarningSign text="This is a danger alert!" />
     </div>
   );
 }
