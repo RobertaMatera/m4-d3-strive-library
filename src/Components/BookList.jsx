73abed7adec93eaa61062aeb5329fb3141858396
RobertaMatera <materaroberta11@gmail.com>
2022-01-11 16:33:12 +0100

    BookList component with hooks

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -6,67 +8,65 @@ import Form from "react-bootstrap/Form";
 import SingleBook from "./SingleBook";
 import CommentArea from "./CommentArea";
 
-class BookList extends React.Component {
-  state = {
+const BookList = ({books}) => {
+  const [bookState, setBookState] = useState({
     searchQuery: "",
     selectedBook: null,
-  };
+  });
 
-  render() {
-    return (
-      <Container fluid className="my-3">
-        <Row>
-          <Col md={10}>
-            <Row className="d-flex justify-content-center">
-              <Col md={4}>
-                <Form.Group controlId="formSearch">
-                  <Form.Label>
-                    {" "}
-                    <b>SEARCH</b>{" "}
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Search here"
-                    value={this.state.searchQuery}
-                    onChange={(e) =>
-                      this.setState({ searchQuery: e.target.value })
+  return (
+    <Container fluid className="my-3">
+      <Row>
+        <Col md={10}>
+          <Row className="d-flex justify-content-center">
+            <Col md={4}>
+              <Form.Group controlId="formSearch">
+                <Form.Label>
+                  {" "}
+                  <b>SEARCH</b>{" "}
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Search here"
+                  value={bookState.searchQuery}
+                  onChange={(e) =>
+                    setBookState({ searchQuery: e.target.value })
+                  }
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center">
+            {books
+              .filter((book) =>
+                book.title.toLowerCase().includes(bookState.searchQuery)
+              )
+              .map((book) => (
+                <Col
+                  xs={12}
+                  md={3}
+                  lg={2}
+                  className="my-2 p-0 mx-1 d-flex justify-content-center"
+                  key={book.asin}
+                >
+                  <SingleBook
+                    book={book}
+                    selectedBook={bookState.selectedBook}
+                    changeSelectedBook={(asin) =>
+                      setBookState({ selectedBook: asin })
                     }
                   />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row className="d-flex justify-content-center">
-              {this.props.books
-                .filter((book) =>
-                  book.title.toLowerCase().includes(this.state.searchQuery)
-                )
-                .map((book) => (
-                  <Col
-                    xs={12}
-                    md={3}
-                    lg={2}
-                    className="my-2 p-0 mx-1 d-flex justify-content-center"
-                    key={book.asin}
-                  >
-                    <SingleBook
-                      book={book}
-                      selectedBook={this.state.selectedBook}
-                      changeSelectedBook={(asin) =>
-                        this.setState({ selectedBook: asin })
-                      }
-                    />
-                  </Col>
-                ))}
-            </Row>
-          </Col>
-          <Col md={2} className="d-flex flex-column align-items-start px-0">
-            <h4 className="my-4">Comments</h4>
-            <CommentArea asin={this.state.selectedBook} />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+                </Col>
+              ))}
+          </Row>
+        </Col>
+        <Col md={2} className="d-flex flex-column align-items-start px-0">
+          <h4 className="my-4">Comments</h4>
+          <CommentArea asin={bookState.selectedBook} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default BookList;
