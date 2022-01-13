2a5865eecca2d2cc568bc84b00cb0c2354063034
RobertaMatera <materaroberta11@gmail.com>
2022-01-13 15:29:18 +0100

    Form displayed in Registration component

--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -3,28 +3,69 @@ import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 const Registration = () => {
   return (
-    <Container fluid>
+    <Container className="my-5">
       <Row>
-        <Col>
+        <Col md={6} className="offset-md-3 my-5">
           <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+            <Row>
+              <Col>
+                <Form.Group controlId="formBasicFirstName">
+                  <Form.Label className="d-flex justify-content-start">
+                    First Name
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter your First Name"
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="formBasicLastName">
+                  <Form.Label className="d-flex justify-content-start">
+                    Last Name
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter your Last Name"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
+            <Row>
+              <Col>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label className="d-flex justify-content-start">
+                    Password
+                  </Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group controlId="formConfirmPassword">
+                  <Form.Label className="d-flex justify-content-start">
+                    Confirm Password
+                  </Form.Label>
+                  <Form.Control
+                    type="confirm password"
+                    placeholder="Confirm Password"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className="py-2 px-3 bg-welcome bg-btn"
+                >
+                  Submit
+                </Button>
+              </Col>
+            </Row>
           </Form>
         </Col>
       </Row>
